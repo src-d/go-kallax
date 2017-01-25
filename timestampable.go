93c66d57ea5e6775d57af8d5f5127cdeb824d11b
Miguel Molina <hi@mvader.me>
2017-01-25 10:16:49 +0100

    Update timestampable.go

--- timestampable.go
+++ timestampable.go
@@ -2,16 +2,16 @@ package kallax
 
 import "time"
 
-// Timestamp modelates an object that knows about when was created and updated
+// Timestamp modelates an object that knows about when was created and updated.
 type Timestamps struct {
-	// CreatedAt is the time where the object was created
+	// CreatedAt is the time where the object was created.
 	CreatedAt time.Time
-	// UpdatedAt is the time where the object was updated
+	// UpdatedAt is the time where the object was updated.
 	UpdatedAt time.Time
 	now       func() time.Time
 }
 
-// Timestamp updates the UpdatedAt and creates a new CreatedAt if it does not exist
+// Timestamp updates the UpdatedAt and creates a new CreatedAt if it does not exist.
 func (t *Timestamps) Timestamp() {
 	if t.now == nil {
 		t.now = time.Now
@@ -24,9 +24,8 @@ func (t *Timestamps) Timestamp() {
 	t.UpdatedAt = t.now()
 }
 
-// BeforePersist runs all actions that must be performed before the persist
-// - Timestamp
-func (t *Timestamps) BeforePersist() error {
+// BeforeSave updates the UpdatedAt and creates a new CreatedAt if it does not exist.
+func (t *Timestamps) BeforeSave() error {
 	t.Timestamp()
 	return nil
 }
