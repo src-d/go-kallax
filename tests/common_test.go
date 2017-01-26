843f3403c6138e776d603097988b8073cce7d2d4
David Pordomingo <David.Pordomingo.F@gmail.com>
2017-01-26 14:52:49 +0100

    Add Setup/TearDown-Tests and FailNow if DB can not be accessed

--- tests/common_test.go
+++ tests/common_test.go
@@ -27,39 +27,65 @@ func (s *BaseTestSuite) SetupSuite() {
 		"postgres",
 		fmt.Sprintf(connectionString, user, password, host, database),
 	)
+
 	if err != nil {
 		panic(fmt.Sprintf("It was unable to connect to the DB.\n%s\n", err))
 	}
 
 	s.db = db
 
+	if !s.resetSchema() {
+		s.Require().FailNow("Tests can not be run because database Schema can not be accessed")
+	}
+}
+
+func (s *BaseTestSuite) TearDownSuite() {
+	s.db.Close()
+}
+
+func (s *BaseTestSuite) SetupTest() {
 	if len(s.initQueries) > 0 {
 		s.QuerySucceed(s.initQueries...)
 	}
 }
 
-func (s *BaseTestSuite) TearDownSuite() {
-	s.QuerySucceed(
+func (s *BaseTestSuite) TearDownTest() {
+	s.resetSchema()
+}
+
+func (s *BaseTestSuite) QuerySucceed(queries ...string) bool {
+	success := true
+	for _, query := range queries {
+		res, err := s.db.Exec(query)
+		assert1 := s.NotNil(res, "Resulset should not be empty")
+		assert2 := s.Nil(err, fmt.Sprintf("%s\nshould succeed but it failed.\n%s\n", query, err))
+		if !assert1 || !assert2 {
+			success = false
+		}
+	}
+
+	return success
+}
+
+func (s *BaseTestSuite) QueryFails(queries ...string) bool {
+	success := true
+	for _, query := range queries {
+		res, err := s.db.Exec(query)
+		assert1 := s.Nil(res, "Resulset should be empty but it was not")
+		assert2 := s.NotNil(err, fmt.Sprintf("%s\nshould fail but it succeed", query))
+		if !assert1 || !assert2 {
+			success = false
+		}
+	}
+
+	return success
+}
+
+func (s *BaseTestSuite) resetSchema() bool {
+	return s.QuerySucceed(
 		fmt.Sprintf(`DROP SCHEMA %s CASCADE;`, database),
 		fmt.Sprintf(`CREATE SCHEMA %s;`, database),
 	)
-	s.db.Close()
-}
-
-func (s *BaseTestSuite) QuerySucceed(queries ...string) {
-	for _, query := range queries {
-		res, err := s.db.Exec(query)
-		s.NotNil(res, "Resulset should not be empty")
-		s.Nil(err, fmt.Sprintf("%s\nshould succeed but it failed.\n%s\n", query, err))
-	}
-}
-
-func (s *BaseTestSuite) QueryFails(queries ...string) {
-	for _, query := range queries {
-		res, err := s.db.Exec(query)
-		s.Nil(res, "Resulset should be empty but it was not")
-		s.NotNil(err, fmt.Sprintf("%s\nshould fail but it succeed", query))
-	}
 }
 
 func envOrDefault(key string, def string) string {
