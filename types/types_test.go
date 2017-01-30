dcda3a7fd40d5b4855d15cbeebbfc20a09083209
Miguel Molina <hi@mvader.me>
2017-01-30 14:59:45 +0100

    Tests for types.Nullable

--- types/types_test.go
+++ types/types_test.go
@@ -1,8 +1,11 @@
 package types
 
 import (
+	"fmt"
 	"net/url"
+	"reflect"
 	"testing"
+	"time"
 
 	"github.com/lib/pq"
 	"github.com/stretchr/testify/require"
@@ -91,4 +94,97 @@ func TestArray(t *testing.T) {
 	v, err := arr.Value()
 	require.Nil(err)
 	require.Equal(input, v)
+}
+
+func TestNullable(t *testing.T) {
+	var (
+		Str      string
+		Int64    int64
+		Float64  float64
+		Bool     bool
+		Time     time.Time
+		Duration time.Duration
+	)
+	tim := time.Now().UTC()
+	tim = time.Date(tim.Year(), tim.Month(), tim.Day(), tim.Hour(), tim.Minute(), tim.Second(), 0, tim.Location())
+	s := require.New(t)
+	cases := []struct {
+		name         string
+		typ          string
+		nonNullInput interface{}
+		dst          interface{}
+	}{
+		{
+			"string",
+			"text",
+			"foo",
+			&Str,
+		},
+		{
+			"int64",
+			"bigint",
+			int64(1),
+			&Int64,
+		},
+		{
+			"float64",
+			"decimal",
+			float64(.5),
+			&Float64,
+		},
+		{
+			"bool",
+			"bool",
+			true,
+			&Bool,
+		},
+		{
+			"time.Duration",
+			"bigint",
+			3 * time.Second,
+			&Duration,
+		},
+		{
+			"time.Time",
+			"timestamptz",
+			tim,
+			&Time,
+		},
+	}
+
+	db, err := openTestDB()
+	s.Nil(err)
+
+	defer func() {
+		_, err = db.Exec("DROP TABLE IF EXISTS foo")
+		s.Nil(err)
+		s.Nil(db.Close())
+	}()
+
+	for _, c := range cases {
+		s.Nil(db.QueryRow("SELECT null").Scan(Nullable(c.dst)), c.name)
+		elem := reflect.ValueOf(c.dst).Elem()
+		zero := reflect.Zero(elem.Type())
+		s.Equal(zero.Interface(), elem.Interface(), c.name)
+
+		var input = c.nonNullInput
+		if v, ok := c.nonNullInput.(time.Duration); ok {
+			input = int64(v)
+		}
+
+		_, err := db.Exec(fmt.Sprintf(`CREATE TABLE foo (
+			testcol %s
+		)`, c.typ))
+		s.Nil(err, c.name)
+
+		_, err = db.Exec("INSERT INTO foo (testcol) VALUES ($1)", input)
+		s.Nil(err, c.name)
+
+		s.Nil(db.QueryRow("SELECT testcol FROM foo").Scan(Nullable(c.dst)), c.name)
+		elem = reflect.ValueOf(c.dst).Elem()
+		s.Equal(c.nonNullInput, elem.Interface(), c.name)
+
+		_, err = db.Exec("DROP TABLE foo")
+		s.Nil(err, c.name)
+	}
 }
