95192144e29a2e40f8ce800cc8f9d775e2557463
kamichidu <c.kamunagi@gmail.com>
2017-12-12 22:04:08 +0900

    fix #245 Reload() functions leaks connection
    Add some tests for generated store functions to check connections are
    leaked or not.

--- tests/resultset_test.go
+++ tests/resultset_test.go
@@ -128,6 +128,7 @@ func (s *ResulsetSuite) TestResultSetForEachError() {
 
 	s.NotPanics(func() {
 		rs := store.MustFind(NewResultSetFixtureQuery())
+		defer rs.Close()
 		err := rs.ForEach(func(*ResultSetFixture) error {
 			return fail
 		})
